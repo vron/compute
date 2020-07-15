f8fe8939cfe0088ba7d4d7e010098eabec9ffdb7
Viktor Ogeman <viktor.ogeman@gmail.com>
2020-07-15 21:50:16 +0200

    use std::thread instead of posix

--- runtime/runtime.hpp
+++ runtime/runtime.hpp
@@ -2,24 +2,24 @@
 
 #include "debug.hpp"
 #include <cstring>
+#include <thread>
 
 struct thread_data {
   pthread_barrier_t *barrier;
-  pthread_t thread;
+  std::thread thread;
   shader *kernel;
 };
 
-void *thread_core(void *ptr) {
-  struct thread_data *td = (struct thread_data *)(ptr);
-  cpt_log("starting thread %p", td);
-
+void thread_core(struct thread_data *td) {
   td->kernel->thread = td;
+  cpt_log("thread calling main %p", td);
   td->kernel->main();
-  return NULL;
+  cpt_log("main called %p", td);
+  return;
 }
 
 void shader::barrier() {
-  cpt_trace("barrier call %p", this);
+  cpt_trace("barrier call %p", this->thread->barrier);
   pthread_barrier_wait(this->thread->barrier);
   cpt_trace("barrier release %p", this);
 }
@@ -32,6 +32,8 @@ struct kernel {
 
   kernel(int32_t num_t) {
     if (num_t < 1) {
+      this->set_error(EINVAL, "must use at least 1 thread");
+      return;
     }
     this->num_threads = (unsigned int)num_threads;
   }
@@ -66,26 +68,20 @@ struct error_t kernel::dispatch(cpt_data d, int32_t nx, int32_t ny,
   if (!this->ensure_alignments(d))
     return this->error();
 
-  struct thread_data *threads;
-  threads = (struct thread_data *)malloc(sizeof(*threads) * _cpt_WG_SIZE_Z *
-                                         _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X);
+  struct thread_data threads[_cpt_WG_SIZE_Z * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X];
   pthread_barrier_t barrier;
 
-  uint32_t numx = (uint32_t)nx;
-  uint32_t numy = (uint32_t)ny;
-  uint32_t numz = (uint32_t)nz;
-  for (uint32_t gz = 0; gz < numz; ++gz) {
-    for (uint32_t gy = 0; gy < numy; ++gy) {
-      for (uint32_t gx = 0; gx < numx; ++gx) {
+  for (uint32_t gz = 0; gz < (uint32_t)nz; ++gz) {
+    for (uint32_t gy = 0; gy < (uint32_t)ny; ++gy) {
+      for (uint32_t gx = 0; gx < (uint32_t)nx; ++gx) {
 
-        int ercc = pthread_barrier_init(
-            &barrier, NULL, _cpt_WG_SIZE_Z * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X);
-        cpt_log("init %d %p", ercc, &barrier);
-        if (ercc) {
-          this->set_error(ercc, "error creating thread barrier");
-          return this->error();
-        }
-
+  int ercc = pthread_barrier_init(
+      &barrier, NULL, _cpt_WG_SIZE_Z * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X);
+  cpt_log("init %d %p", ercc, &barrier);
+  if (ercc) {
+    this->set_error(ercc, "error creating thread barrier");
+    return this->error();
+  }
         auto sd = shader::create_shared_data();
 
         for (uint32_t lz = 0; lz < _cpt_WG_SIZE_Z; ++lz) {
@@ -93,13 +89,8 @@ struct error_t kernel::dispatch(cpt_data d, int32_t nx, int32_t ny,
             for (uint32_t lx = 0; lx < _cpt_WG_SIZE_X; ++lx) {
               long index = lx + ly * _cpt_WG_SIZE_X +
                            lz * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X;
-              cpt_verbose("in loop %d %ld",
-                          _cpt_WG_SIZE_Z * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X,
-                          index);
-              shader *k = new shader(); // TODO: re-use the
-                                        // allocation across calls
+              shader *k = new shader();
 
-              // TODO: can we avoid all these calcs?
               k->gl_NumWorkGroups.z = nz;
               k->gl_NumWorkGroups.y = ny;
               k->gl_NumWorkGroups.x = nx;
@@ -125,48 +116,44 @@ struct error_t kernel::dispatch(cpt_data d, int32_t nx, int32_t ny,
               threads[index].kernel = k;
               threads[index].barrier = &barrier;
 
-              // for each one, create a thread that we will use:
-              int no;
-              cpt_verbose("about to creae");
-              no = pthread_create(&threads[index].thread, NULL, thread_core,
-                                  (void *)&threads[index]);
-              cpt_verbose("thread create no %d", no);
-              if (no) {
-                this->set_error(no, "error creating threads");
-                return this->error();
-                // TODO: memory leas... (this entire functin...)
+              try {
+                cpt_verbose("about to create %p", &threads[index]);
+                threads[index].thread =
+                    std::thread(thread_core, &threads[index]);
+              } catch (...) {
+                cpt_log("ERROR CRATING thread");
               }
+              cpt_verbose("thread created now");
             }
           }
         }
 
+        cpt_verbose("loop done");
         // wait for all the threads to finish and join them
         for (uint32_t lz = 0; lz < _cpt_WG_SIZE_Z; ++lz) {
           for (uint32_t ly = 0; ly < _cpt_WG_SIZE_Y; ++ly) {
             for (uint32_t lx = 0; lx < _cpt_WG_SIZE_X; ++lx) {
               long index = lx + ly * _cpt_WG_SIZE_X +
                            lz * _cpt_WG_SIZE_Y * _cpt_WG_SIZE_X;
-              int s = pthread_join(threads[index].thread, NULL);
-              cpt_log("ERC %d", s);
-              if (s != 0) {
-                this->set_error(s, "error joining threads");
-                return this->error();
+
+              cpt_verbose("about to join %ld", index);
+              try {
+                threads[index].thread.join();
+              } catch (...) {
+                cpt_log("ERROR JOINING thread");
               }
-              // return error("error joining threads: %d", s);
+              cpt_log("joined");
               free(threads[index].kernel);
             }
           }
         }
-
+        cpt_log("wg done");
         shader::free_shared_data(sd);
 
-        // TODO: Free!
         pthread_barrier_destroy(&barrier);
       }
     }
   }
-
-  free(threads);
 
   return this->error();
 }
