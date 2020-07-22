17a3bf291de43d71fc88a5349ad25106847bfc55
Viktor Ogeman <viktor.ogeman@gmail.com>
2020-07-22 10:00:54 +0200

    Implement a lock-less barrier for performance
    name           old time/op  new time/op  delta
    Scheduling     9.71ms ±35%  9.29ms ±35%     ~     (p=0.393 n=10+10)
    Scheduling-8   66.0ms ± 1%  28.3ms ± 5%  -57.07%  (p=0.000 n=10+10)
    TransTri       4.60ms ± 3%  4.62ms ± 2%     ~     (p=0.515 n=8+10)
    TransTri-8     7.45ms ± 1%  6.59ms ± 4%  -11.57%  (p=0.000 n=10+10)
    TransTriRef    15.0ms ± 3%  15.1ms ± 3%     ~     (p=0.739 n=10+10)
    TransTriRef-8  2.08ms ± 0%  2.09ms ± 1%   +0.64%  (p=0.003 n=9+10)
    Trig           18.5ms ± 1%  17.7ms ± 0%   -4.36%  (p=0.000 n=9+9)
    Trig-8         3.46ms ± 1%  3.44ms ± 3%     ~     (p=0.393 n=10+10)
    TrigRef        41.7ms ± 1%  41.9ms ± 2%     ~     (p=0.123 n=10+10)
    TrigRef-8      5.61ms ± 0%  5.66ms ± 1%   +0.78%  (p=0.000 n=9+9)

--- runtime/routines/barrier.hpp
+++ runtime/routines/barrier.hpp
@@ -1,40 +1,44 @@
 #pragma once
 #include <condition_variable>
 #include <mutex>
+#include <cstdint>
 
-// a barrier which forces multiple threads to wait at that point until a pre-set
-// number of threads has reached the barrier. Once activated it is re-set and can
-// be used again.
-// Should be replaced by std:barrier once that is implemented in clang.
+// Barrier uses busy-wait to improve performance of sync between threads in the same
+// work group.
 class Barrier {
 private:
-  std::mutex m;
-  std::condition_variable cv;
-  int count;
-  int gen;
-  int original_count;
+// we store both the count and the generation in the same variable
+// such that we can load and store both in one atomic operation.
+  uint32_t count;
+  uint32_t original_count;
 
 public:
-  Barrier() : count(0), gen(0), original_count(0){};
+  Barrier() : count(0), original_count(0){};
 
+  // the caller must ensure that there us synchronization betwen a call to set and any call to wait.
   void set_count(int c) {
-    std::unique_lock<std::mutex> lc(m);
-    assert(this->count == this->original_count);
-    count = c;
-    original_count = c;
+    __atomic_store_n(&count, c, __ATOMIC_SEQ_CST);
+    __atomic_store_n(&original_count, c, __ATOMIC_SEQ_CST);
   }
 
   void wait() {
-    std::unique_lock<std::mutex> lc(m);
-    int tempgen = gen;
-    count--;
-    if (count == 0) {
-      gen++;
-      count = original_count;
-      lc.unlock();
-      cv.notify_all();
+    uint32_t val = __atomic_fetch_sub(&count, 1, __ATOMIC_SEQ_CST);
+    uint32_t my_count = val & 0xFFFF;
+    uint32_t my_gen = val & 0xFFFF0000;
+    if ( my_count == 1) {
+      // we are the last one to reach the barrier, to signal the others
+      my_gen += 1 << 0x10;
+      uint32_t oc = __atomic_load_n(&original_count, __ATOMIC_SEQ_CST);
+      oc += my_gen;
+      __atomic_store_n(&count, oc, __ATOMIC_SEQ_CST);
+      return;
     } else {
-      cv.wait(lc, [tempgen, this] { return tempgen != this->gen; });
+      uint32_t wait_gen;
+      do {
+        uint32_t wait_val = __atomic_load_n(&this->count, __ATOMIC_SEQ_CST);
+        wait_gen = wait_val & 0xFFFF0000;
+      } while(wait_gen == my_gen);
+      return;
     }
   };
 };
